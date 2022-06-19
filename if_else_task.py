f376dd6fadaaabe33f99b0fd70df188217faa4a3
Sheraz <sherazsafdar00@gmail.com>
2022-06-19 14:00:23 +0500

    solve

--- if_else_task.py
+++ if_else_task.py
@@ -2,11 +2,10 @@ def evenodd(a):
     if a%2==0:
         if a>=2 and a<=5:
             return "not weird"
-        elif:
-            if a>=6 and a<=20:
-                return "weird"
-            if a>20:
-                return "not weird"
+        elif a>=6 and a<=20:
+            return "weird"
+        elif a>20:
+            return "not weird"
     else:
         return "weird"                     
 a = int(input("Please enter number : "))
